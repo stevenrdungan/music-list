5c68378478432d0b0b483dc037ca3fffda19dc52
= <=>
2018-01-14 12:48:18 -0500

    login feature

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, request, url_for
+from flask_login import current_user, login_user, logout_user, login_required
 from app import app
 from app.forms import LoginForm
+from app.models import User
 import pandas as pd
 import numpy as np
 
@@ -14,6 +16,7 @@ def prep_table(df):
 @app.route('/')
 @app.route('/index')
 @app.route('/favorites')
+@login_required
 def index():
     df = pd.read_csv('./app/input_files/favorites.txt', sep='\t')
     df.drop(['Rank'], axis=1, inplace=True)
@@ -38,9 +41,22 @@ def tolisten():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect('/index')
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or not next_page.startswith('/'):
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
